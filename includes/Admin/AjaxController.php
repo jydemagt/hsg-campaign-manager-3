f8e657e3bfe5d54a6bacc41eee41b492018f6b18
jydemagt <67008158+jydemagt@users.noreply.github.com>
2026-07-07 14:03:13 +0200

    Update AjaxController.php

--- includes/Admin/AjaxController.php
+++ includes/Admin/AjaxController.php
@@ -111,10 +111,12 @@ final class AjaxController {
 				'end_date'   => sanitize_text_field( wp_unslash( $_POST['end_date'] ?? '' ) ),
 				'priority'   => absint( $_POST['priority'] ?? 10 ),
 				'products'   => $products,
-				'type'       => 'fixed_price',
-				'value'      => '',
-				'coupon'     => '',
-				'stackable'  => false,
+
+				// Nye felter
+				'type'       => sanitize_key( wp_unslash( $_POST['type'] ?? 'fixed_price' ) ),
+				'value'      => wc_format_decimal( wp_unslash( $_POST['value'] ?? '' ) ),
+				'coupon'     => sanitize_text_field( wp_unslash( $_POST['coupon'] ?? '' ) ),
+				'stackable'  => ! empty( $_POST['stackable'] ),
 			)
 		);
 
